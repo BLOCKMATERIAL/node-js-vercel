82d356f3842190276ebe8ddb048298b7eb93061d
Myroslav Hryschenko <miroslavgrischenko@gmail.com>
2023-11-30 13:33:31 +0200

    fix

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const tunnel = require("./routes/tunnel");
 
 // Middlewares
 const app = express();
-app.use(express.json());
-app.use(cors())
+app.use(express.json(), cors());
 // Routes
 app.use("/home", tunnel);
 
